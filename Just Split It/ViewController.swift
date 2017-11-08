b5c9e7602ff4ad92431e555771fd663f22642ed8
Maria-Belem <Maria-Belem@172-17-23-63.dynapool.nyu.edu>
2017-11-08 17:19:04 -0500

    Initial Commit

---
+++ Just Split It/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Just Split It
+//
+//  Created by Maria-Belem on 11/8/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
