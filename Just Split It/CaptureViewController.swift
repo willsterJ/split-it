e7ff4a55fe5199fc69b7fe718f498bf7b936b1d3
Muhammad Joyo <muhammadjoyo@nyu.edu>
2017-12-09 23:57:59 -0500

    Added CaptureViewController

---
+++ Just Split It/CaptureViewController.swift
@@ -0,0 +1,34 @@
+//
+//  CaptureViewController.swift
+//  Just Split It
+//
+//  Created by Muhammad Joyo on 12/9/17.
+//
+
+import UIKit
+
+class CaptureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
