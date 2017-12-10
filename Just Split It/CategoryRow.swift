89c80b905491729a783d430b16dbbd2d6f47d343
William <banana@williams-macbook-air.local>
2017-12-10 18:18:29 -0500

    Individual Bill for each collection view cell

--- Just Split It/CategoryRow.swift
+++ Just Split It/CategoryRow.swift
@@ -10,7 +10,8 @@ import UIKit
 class CategoryRow: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     var friendArray = [Friend]() // holds the friend array from Bill in GroupBillVC
-
+    var nameLabel: UILabel = UILabel()
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // test
         print ("category friend count: " + friendArray.count.description)
@@ -21,6 +22,18 @@ class CategoryRow: UITableViewCell, UICollectionViewDataSource, UICollectionView
     // extension: UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let friendCellCollection = collectionView.dequeueReusableCell(withReuseIdentifier: "friendCellCollection", for: indexPath)
+        
+        self.nameLabel.frame = CGRect(x: 0, y: 0, width: friendCellCollection.frame.width, height: friendCellCollection.frame.height)
+        
+        friendCellCollection.contentView.addSubview(nameLabel)
+        
+        for i in 1...self.friendArray.count{
+            if (indexPath.row == i){
+                self.nameLabel.text = self.friendArray[indexPath.row].name
+            }
+        }
+        
+
         
         
         return friendCellCollection
