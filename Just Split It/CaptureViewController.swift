87c1ac1bd916a4163440053ef8d7a4798c64d742
Muhammad Joyo <muhammadjoyo@nyu.edu>
2017-12-12 02:30:09 -0500

    Created PictureViewController and cleaned up some code in CaptureViewController

--- Just Split It/CaptureViewController.swift
+++ Just Split It/CaptureViewController.swift
@@ -21,9 +21,6 @@ class CaptureViewController: UIViewController {
 	//Allows us to capture a still image from the captureSession
 	var capturePhotoOutput: AVCapturePhotoOutput?
 	
-	//Array of generic VNRequests
-	//var requests = [VNRequest]()
-	
     override func viewDidLoad() {
         super.viewDidLoad()
 		
@@ -66,11 +63,6 @@ class CaptureViewController: UIViewController {
         // Dispose of any resources that can be recreated.
 	}
 	
-	override func viewWillAppear(_ animated: Bool) {
-		//startLiveVideo()
-		//startTextDetection()
-	}
-	
 	@IBAction func onTapTakePhoto(_ sender: Any) {
 		// Check if capturePhotoOutput is valid
 		guard let capturePhotoOutput = self.capturePhotoOutput else {
@@ -88,152 +80,6 @@ class CaptureViewController: UIViewController {
 		//Call capturePhoto method and pass photoSettings and self which implements AVCapturePhotoCaptureDelegate
 		capturePhotoOutput.capturePhoto(with: photoSettings, delegate: self)
 	}
-	
-//	override func viewDidLayoutSubviews() {
-//		imageView.layer.sublayers?[0].frame = imageView.bounds
-//	}
-	
-//	//Connect the session to our device
-//	func startLiveVideo() {
-//		//Modify the settings of our session
-//		session.sessionPreset = AVCaptureSession.Preset.photo
-//		//Set AVMediaType as video
-//		let captureDevice = AVCaptureDevice.default(for: AVMediaType.video)
-//
-//		//Define device input and output
-//		let deviceInput = try! AVCaptureDeviceInput(device: captureDevice!)
-//		let deviceOutput = AVCaptureVideoDataOutput()
-//		//Set video format
-//		deviceOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA)]
-//		deviceOutput.setSampleBufferDelegate(self, queue: DispatchQueue.global(qos: DispatchQoS.QoSClass.default))
-//		//Add input and output to the session
-//		session.addInput(deviceInput)
-//		session.addOutput(deviceOutput)
-//
-//		//Add a sublayer containing the video preview to the imageview
-//		let imageLayer = AVCaptureVideoPreviewLayer(session: session)
-//		imageLayer.frame = imageView.bounds
-//		imageView.layer.addSublayer(imageLayer)
-//
-//		//Start running the session
-//		session.startRunning()
-//
-//	}
-//
-//	func startTextDetection() {
-//		//type of VNRequest that looks for rectangles with some text in them
-//		//When the request is completed, call detectTextHandler
-//		let textRequest = VNDetectTextRectanglesRequest(completionHandler: self.detectTextHandler)
-//		//tells the framework to give us exacly what it recognized
-//		textRequest.reportCharacterBoxes = true
-//		self.requests = [textRequest]
-//
-//	}
-//
-//	func detectTextHandler(request: VNRequest, error: Error?) {
-//		//create observations which will contain the results of the request
-//		guard let observations = request.results else {
-//			print("No Result")
-//			return
-//		}
-//		//Go through all the results of request and transform them into VNTextObservation and set them to result
-//		let result = observations.map({$0 as? VNTextObservation})
-//
-//		//Run code asynchronously
-//		DispatchQueue.main.async() {
-//			//remove bottom most layer in imageview
-//			self.imageView.layer.sublayers?.removeSubrange(1...)
-//
-//			for region in result {
-//				//check if region exists in the result from the textObservation
-//				guard let rg = region else {
-//					continue
-//				}
-//
-//				self.highlightWord(box: rg)
-//
-//				//Check if there are character boxes
-//				if let boxes = region?.characterBoxes {
-//					for characterBox in boxes {
-//						self.highlightLetters(box: characterBox)
-//					}
-//				}
-//			}
-//		}
-//	}
-//
-//	func highlightWord(box: VNTextObservation) {
-//		guard let boxes = box.characterBoxes else {
-//			return
-//		}
-//
-//		var maxX: CGFloat = 9999.0
-//		var minX: CGFloat = 0.0
-//		var maxY: CGFloat = 9999.0
-//		var minY: CGFloat = 0.0
-//
-//		for char in boxes {
-//			if(char.bottomLeft.x < maxX) {
-//				maxX = char.bottomLeft.x
-//			}
-//			if(char.bottomRight.x > minX) {
-//				minX = char.bottomRight.x
-//			}
-//			if(char.bottomRight.y < maxY) {
-//				maxY = char.bottomRight.y
-//			}
-//			if(char.topRight.y > minY) {
-//				minY = char.topRight.y
-//			}
-//		}
-//
-//		let xCord = maxX * imageView.frame.size.width
-//		let yCord = (1 - minY) * imageView.frame.size.height
-//		let width = (minX - maxX) * imageView.frame.size.width
-//		let height = (minY - maxY) * imageView.frame.size.height
-//
-//		let outline =  CALayer()
-//		outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
-//		outline.borderWidth = 2.0
-//		outline.borderColor = UIColor.red.cgColor
-//
-//		imageView.layer.addSublayer(outline)
-//	}
-//
-//	func highlightLetters(box: VNRectangleObservation) {
-//		let xCord = box.topLeft.x * imageView.frame.size.width
-//		let yCord = (1 - box.topLeft.y) * imageView.frame.size.height
-//		let width = (box.topRight.x - box.bottomLeft.x) * imageView.frame.size.width
-//		let height = (box.topLeft.y - box.bottomLeft.y) * imageView.frame.size.height
-//
-//		let outline = CALayer()
-//		outline.frame = CGRect(x: xCord, y: yCord, width: width, height: height)
-//		outline.borderWidth = 1.0
-//		outline.borderColor = UIColor.blue.cgColor
-//
-//		imageView.layer.addSublayer(outline)
-//	}
-//
-//	//adopts the AVCaptureVideoDataOutputSampleBufferDelegate protocol
-//	func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
-//		guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-//			return
-//		}
-//
-//		var requestOptions:[VNImageOption : Any] = [:]
-//
-//		if let camData = CMGetAttachment(sampleBuffer, kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, nil) {
-//			requestOptions = [.cameraIntrinsics:camData]
-//		}
-//
-//		let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation(rawValue: 6)!, options: requestOptions)
-//
-//		do {
-//			try imageRequestHandler.perform(self.requests)
-//		} catch {
-//			print(error)
-//		}
-//	}
 
 }
 extension CaptureViewController : AVCapturePhotoCaptureDelegate {
@@ -244,18 +90,16 @@ extension CaptureViewController : AVCapturePhotoCaptureDelegate {
 					 bracketSettings: AVCaptureBracketedStillImageSettings?,
 					 error: Error?) {
 		//Get captured image
-		// Make sure we get some photo sample buffer
-		guard error == nil,
-			let photoSampleBuffer = photoSampleBuffer else {
-				print("Error capturing photo: \(String(describing: error))")
-				return
+		//Make sure we get the photoSampleBuffer
+		guard error == nil, let photoSampleBuffer = photoSampleBuffer else {
+			print("Error capturing photo: \(String(describing: error))")
+			return
 		}
-		// Convert photo same buffer to a jpeg image data by using // AVCapturePhotoOutput
-		guard let imageData =
-			AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else {
-				return
+		//Convert photoSampleBuffer to a jpeg
+		guard let imageData = AVCapturePhotoOutput.jpegPhotoDataRepresentation(forJPEGSampleBuffer: photoSampleBuffer, previewPhotoSampleBuffer: previewPhotoSampleBuffer) else {
+			return
 		}
-		// Initialise a UIImage with our image data
+		//Init a UIImage with our image data
 		let capturedImage = UIImage.init(data: imageData , scale: 1.0)
 		if let image = capturedImage {
 			// Save our captured image to photos album
---
+++ Just Split It/PictureViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PictureViewController.swift
+//  Just Split It
+//
+//  Created by Muhammad Joyo on 12/12/17.
+//
+
+import UIKit
+
+class PictureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+	
+	
+
+}
